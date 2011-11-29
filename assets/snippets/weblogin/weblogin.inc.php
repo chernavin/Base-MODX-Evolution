3ca3528a50dfb746b764ff1e189a33e101d41381
Alexander Chernavin <chernavin.a.a@mail.ru>
2011-11-29 15:46:52 +0400

    Установлена модификация WebLogin от Andchir

--- assets/snippets/weblogin/weblogin.inc.php
+++ assets/snippets/weblogin/weblogin.inc.php
@@ -24,7 +24,7 @@ if(!isset($_SESSION['webValidated'])){
 //        $thestring = $rc4->endecrypt($thepasswd,$sitename,'de');
 //        $uid = $thestring;
 //    }else{
-        $uid = isset($_POST['username'])? $modx->db->escape(htmlspecialchars(trim($_POST['username']), ENT_QUOTES)):'';
+        $uid = isset($_POST['username'])? $modx->db->escape(htmlspecialchars($_POST['username'], ENT_QUOTES)):'';
 //    }
     ?>
     <script type="text/JavaScript">
@@ -86,11 +86,13 @@ if(!isset($_SESSION['webValidated'])){
         $tpl = str_replace("[+checkbox+]",(isset($cookieSet) ? "checked" : ""),$tpl);
         $tpl = str_replace("[+logintext+]",$loginText,$tpl);    
         echo $tpl;
+    /*
     ?>
     <script type="text/javascript">
-        if (document.loginfrm) <?php echo !empty($uid) ? "document.loginfrm.password.focus()" : "document.loginfrm.username.focus()" ?>;
+        //if (document.loginfrm) <?php echo !empty($uid) ? "document.loginfrm.password.focus()" : "document.loginfrm.username.focus()" ?>;
     </script>
     <?php
+    */
     $output .= ob_get_contents();
     ob_end_clean();
 } else {
@@ -135,15 +137,15 @@ function getWebLogintpl(){
     <td>
     <table border="0" cellspacing="0" cellpadding="0">
       <tr>
-        <td><b>User:</b></td>
+        <td><b>Логин:</b></td>
         <td><input type="text" name="username" tabindex="1" onkeypress="return webLoginEnter(document.loginfrm.password);" size="8" style="width: 150px;" value="[+username+]" /></td>
       </tr>
       <tr>
-        <td><b>Password:</b></td>
+        <td><b>Пароль:</b></td>
         <td><input type="password" name="password" tabindex="2" onkeypress="return webLoginEnter(document.loginfrm.cmdweblogin);" style="width: 150px;" value="" /></td>
       </tr>
       <tr>
-        <td><label for="chkbox" style="cursor:pointer">Remember me:&nbsp; </label></td>
+        <td><label for="chkbox" style="cursor:pointer">Запомнить:&nbsp; </label></td>
         <td>
         <table width="100%"  border="0" cellspacing="0" cellpadding="0">
           <tr>
@@ -155,7 +157,7 @@ function getWebLogintpl(){
         </td>
       </tr>
       <tr>
-        <td colspan="2"><a href="#" onclick="webLoginShowForm(2);return false;">Forget Password?</a></td>
+        <td colspan="2"><a href="#" onclick="webLoginShowForm(2);return false;">Забыли пароль?</a></td>
       </tr>
     </table>
     </td>
@@ -164,21 +166,21 @@ function getWebLogintpl(){
     </form>
     <hr>
     <!-- log out hyperlink section -->
-    <a href='[+action+]'>[+logouttext+]</a>
+    Вход произведён (<a href='[+action+]'>[+logouttext+]</a>).
     <hr>
     <!-- Password reminder form section -->
     <form name="loginreminder" method="post" action="[+action+]" style="margin: 0px; padding: 0px;">
     <input type="hidden" name="txtpwdrem" value="0" />
     <table border="0">
         <tr>
-          <td>Enter the email address of your account <br />below to receive your password:</td>
+          <td>Введите адрес электронной почты <br />чтобы восстановить пароль:</td>
         </tr>
         <tr>
           <td><input type="text" name="txtwebemail" size="24" /></td>
         </tr>
         <tr>
-          <td align="right"><input type="submit" value="Submit" name="cmdweblogin" />
-          <input type="reset" value="Cancel" name="cmdcancel" onclick="webLoginShowForm(1);" /></td>
+          <td align="right"><input type="submit" value="Отправить" name="cmdweblogin" />
+          <input type="reset" value="Отмена" name="cmdcancel" onclick="webLoginShowForm(1);" /></td>
         </tr>
       </table>
     </form>
