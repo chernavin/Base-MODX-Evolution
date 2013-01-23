575ee9ee65f9649a72a182685a4ed9c795aaedb4
Aleksandr Chernavin <chernavin.a.a@mail.ru>
2013-01-23 22:40:08 +0400

    Обновление до 1.0.8 (2)

--- manager/includes/version.inc.php
+++ manager/includes/version.inc.php
@@ -2,4 +2,4 @@
 $modx_version = '1.0.8';           // Current version number
 $modx_release_date = 'Jan 08, 2013'; // Date of release
 $modx_branch = 'Evolution';        // Codebase name
-$modx_full_appname = 'MODx '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
+$modx_full_appname = 'MODX '.$modx_branch.' '.$modx_version.' ('.$modx_release_date.')';
